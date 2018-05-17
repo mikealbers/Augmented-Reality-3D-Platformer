94ffe53bd4d7f39c1b6187edef1760fb6f711353
Michael Albers <albermichael@tutanota.com>
2018-05-17 12:02:30 -0700

    did it

---
+++ 3D Altered Platformer - Copy/Assets/Scripts/CameraController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float rotateSpeed;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool useOffsetValues;
+    public bool invertY;
+    public Transform pivot;
+
+	// Use this for initialization
+	void Start () {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.position;
+        pivot.transform.parent = null;
+        //pivot.transform.parent = target.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        pivot.transform.position = target.transform.position;
+
+        //get x position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivot.Rotate(0, horizontal, 0);
+
+        //Get the Y position of the mouse and rotate the pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        //Limit the up/down camera rotation
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        if (pivot.rotation.eulerAngles.x > 180 && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        //move the camera based on the current rotation of the target and the original offset
+        float desiredYAngle = pivot.eulerAngles.y;
+        float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+
+        //transform.position = target.position - (offset * rotation);
+        //offset * rotation will not work because of Quaternion. I have no idea why.
+        transform.position = target.position - (rotation * offset);
+        
+        //transform.position = target.position - offset;
+        if (transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
+        }
+        transform.LookAt(target);
+	}
+}
---
+++ 3D Altered Platformer - Copy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+    public CharacterController controller;
+    public GameObject playerModel;
+
+    //public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        //rb = GetComponent<Rigidbody> ();
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /*rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }*/
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * CrossPlatformInputManager.GetAxisRaw("Vertical")) + (transform.right * CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+        moveDirection = moveDirection.normalized * moveSpeed;
+        moveDirection.y = yStore;
+
+
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Move the player in different directions based on camera look direction
+        //if (CrossPlatformInputManager.GetAxisRaw("Horizontal") != 0 || CrossPlatformInputManager.GetAxisRaw("Vertical") !=0)
+        //{
+        //    transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+        //    Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0, moveDirection.z));
+        //    playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        //}
+
+        //Change animations
+        anim.SetBool("IsGrounded", controller.isGrounded);
+        anim.SetFloat("Speed", (Mathf.Abs(CrossPlatformInputManager.GetAxisRaw("Vertical")) + Mathf.Abs(CrossPlatformInputManager.GetAxisRaw("Horizontal"))));
+    }
+}
